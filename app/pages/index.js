24ba7bfee3d72a635543c4b6ac70bdcc9bc99ac7
James Gorrie <jamesgorrie@users.noreply.github.com>
2018-01-12 14:23:22 +0000

    Get going

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+import {useful} from 'common/utils';
+const Index = () => (
+  <Layout>
+  <p>Hello {useful()}.</p>
+  </Layout>
+);
+
+export default Index;
---
+++ common/utils.js
@@ -0,0 +1,1 @@
+export const useful = () => 'Useful innit.';
